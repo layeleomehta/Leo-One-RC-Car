0e2431035a511d2a89428746ca54fda88e88fa32
Laye 'Leo' Mehta <32559821+layeleomehta@users.noreply.github.com>
2018-10-17 01:24:11 -0400

    Add files via upload

---
+++ Car_Slave_Code.ino
@@ -0,0 +1,96 @@
+#include<AFMotor.h>
+
+AF_DCMotor motor1(1); //front right wheel
+AF_DCMotor motor2(2); //front left wheel
+AF_DCMotor motor3(3); //back left wheel
+AF_DCMotor motor4(4); //back right wheel
+
+int ascii_reader = 0; 
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(38400);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if(Serial.available() > 0)
+  {
+    ascii_reader = Serial.read(); 
+  }
+
+  
+  if(ascii_reader == '4') //don't move at all
+  {
+    motor1.run(RELEASE); 
+    motor2.run(RELEASE); 
+    motor3.run(RELEASE); 
+    motor4.run(RELEASE); 
+    motor1.setSpeed(0); 
+    motor2.setSpeed(0); 
+    motor3.setSpeed(0); 
+    motor4.setSpeed(0);
+    ascii_reader = 0; 
+    delay(2000); 
+  }
+  
+  else if(ascii_reader == '0') //move fwd
+  {
+    motor1.run(FORWARD); 
+    motor2.run(FORWARD); 
+    motor3.run(FORWARD); 
+    motor4.run(FORWARD);
+    motor1.setSpeed(150); 
+    motor2.setSpeed(150); 
+    motor3.setSpeed(150); 
+    motor4.setSpeed(150); 
+    ascii_reader = 0; 
+    delay(1000);
+  }
+
+  else if(ascii_reader == '1') //move bckwd
+  {
+    motor1.run(BACKWARD); 
+    motor2.run(BACKWARD); 
+    motor3.run(BACKWARD); 
+    motor4.run(BACKWARD);
+    motor1.setSpeed(150); 
+    motor2.setSpeed(150); 
+    motor3.setSpeed(150); 
+    motor4.setSpeed(150); 
+    ascii_reader = 0; 
+    delay(1000);
+  }
+
+  else if(ascii_reader == '2') //move fwd right
+  {
+    motor1.run(RELEASE); //turn off front right motor, so car turns fwd right. 
+    motor2.run(FORWARD); 
+    motor3.run(FORWARD); 
+    motor4.run(FORWARD);
+    motor1.setSpeed(0); 
+    motor2.setSpeed(150); 
+    motor3.setSpeed(150); 
+    motor4.setSpeed(150);
+    ascii_reader = 0; 
+    delay(1000);
+  }
+
+  else if(ascii_reader == '3') //move fwd left
+  {
+    motor1.run(FORWARD); 
+    motor2.run(RELEASE); //turn off front left motor, so car turns fwd right. 
+    motor3.run(FORWARD); 
+    motor4.run(FORWARD);
+    motor1.setSpeed(150); 
+    motor2.setSpeed(0); 
+    motor3.setSpeed(150); 
+    motor4.setSpeed(150);
+    ascii_reader = 0; 
+    delay(1000);
+  }
+
+}
---
+++ Remote__Master__Code.ino
@@ -0,0 +1,43 @@
+int joy_x = 0; 
+int joy_y = 1; 
+int xval; 
+int yval; 
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(38400);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  xval = analogRead(joy_x); 
+  yval = analogRead(joy_y);
+
+  if(yval < 200) //for moving fwd
+  {
+    Serial.write('0');
+  }
+
+  else if(yval > 900) //for moving bckwd
+  {
+    Serial.write('1'); 
+  }
+
+  else if(xval > 900) //for moving right
+  {
+    Serial.write('2'); 
+  }
+
+  else if(xval < 200) //for moving left
+  {
+    Serial.write('3'); 
+  }
+
+  else //for all other cases, don't move. 
+  {
+    Serial.write('4'); 
+  }
+
+  
+}
